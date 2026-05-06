e039ad2b3ebd9135890e8c46fb0685b8b502a2cb
ANDREW <andrJohnson@csumb.edu>
2026-05-05 17:40:46 -0700

    Added basic type effectiveness chart for damage calculations of the primary types

--- src/main/java/FightController.java
+++ src/main/java/FightController.java
@@ -3,11 +3,15 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import java.util.EnumMap;
 
 public class FightController {
     DatabaseManager db = DatabaseManager.getInstance();
     private final User user1 = AppData.getInstance().getDuel().get(0);
     private final User user2 = AppData.getInstance().getDuel().get(1);
+
+
+
 
     public Scene buildScene(){
         Label title = new Label(user1.getName() + " vs " + user2.getName());
@@ -17,5 +21,37 @@ public class FightController {
         layout.setAlignment(Pos.CENTER);
         layout.setPadding(new Insets(16));
         return new Scene(layout, 500, 400);
+
+
+
+    }
+
+    public double getTypeEffectiveness(Pokemon attacker, Pokemon defender) {
+        EnumMap<Poke_Type, EnumMap<Poke_Type, Double>> typeEffectivness = new EnumMap<>(Poke_Type.class);
+
+        //populate enum map
+        //set all to 1 as default
+        for(Poke_Type typeRow: Poke_Type.values()) {
+            typeEffectivness.put(typeRow,new EnumMap<>(Poke_Type.class));
+            for (Poke_Type typeCol : Poke_Type.values()) {
+                typeEffectivness.get(typeRow).put(typeCol, 1.0);
+            }
+        }
+        //Fire
+        typeEffectivness.get(Poke_Type.FIRE).put(Poke_Type.FIRE, 0.5);
+        typeEffectivness.get(Poke_Type.FIRE).put(Poke_Type.WATER, 0.5);
+        typeEffectivness.get(Poke_Type.FIRE).put(Poke_Type.GRASS, 2.0);
+
+        //Water
+        typeEffectivness.get(Poke_Type.WATER).put(Poke_Type.FIRE, 2.0);
+        typeEffectivness.get(Poke_Type.WATER).put(Poke_Type.WATER, 0.5);
+        typeEffectivness.get(Poke_Type.WATER).put(Poke_Type.GRASS, 0.5);
+
+        //Grass
+        typeEffectivness.get(Poke_Type.GRASS).put(Poke_Type.FIRE, 0.5);
+        typeEffectivness.get(Poke_Type.GRASS).put(Poke_Type.WATER, 2.0);
+        typeEffectivness.get(Poke_Type.GRASS).put(Poke_Type.GRASS, 0.5);
+
+        return typeEffectivness.get(attacker.primaryType).get(defender.primaryType);
     }
 }
